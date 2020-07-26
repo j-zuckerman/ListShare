d018c63204db7da42a04f3dad5a0238643ed77f5
James Zuckerman <jzuckerman4@gmail.com>
2020-07-26 16:04:54 -0700

    Updates

---
+++ precache-manifest.f28db1fc4c41bd9197875a5fd20306d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e7432eecb7dc6c1bdf4b00a33c2cb7",
+    "url": "/ListShare/index.html"
+  },
+  {
+    "revision": "3e64c0c6dd6147a03ba5",
+    "url": "/ListShare/static/css/main.4f9f4fb0.chunk.css"
+  },
+  {
+    "revision": "a18baa6a26e4e03a6301",
+    "url": "/ListShare/static/js/2.df62aa17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ListShare/static/js/2.df62aa17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e64c0c6dd6147a03ba5",
+    "url": "/ListShare/static/js/main.5cb71f1d.chunk.js"
+  },
+  {
+    "revision": "65d6605381f9820f2ae9",
+    "url": "/ListShare/static/js/runtime-main.4268e06b.js"
+  }
+]);
